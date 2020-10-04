a0009bacd9dd5042e8187856cc72c751c09a7c2e
geekestLi <3162853966@qq.com>
2020-10-04 18:19:46 +0800

    'addnavinfo'

--- nindex.js
+++ nindex.js
@@ -1,6 +1,26 @@
 window.onload = function(){
+	
+	
+	
 	personInfo = function(){
-		alert()
+		show(1)
+	}
+	
+	
+	
+	newpro = function(){
+		show()
+	}
+	
+	show = function(num=0){
+		let elements = document.getElementsByTagName('iframe')
+		for(let i=0;i<elements.length;i++){		
+			if(num === i+1){
+				document.getElementsByTagName('iframe')[i].style.display = 'block'
+			}else{
+				document.getElementsByTagName('iframe')[i].style.display = 'none'
+			}
+		}
 	}
 }
 	
